e2c92055ca6a46c5e04312c0a08968fe4a6f9765
Chitranjan Gupta <chitranjankumargupta123@gmail.com>
2024-12-11 14:50:05 +0530

    feat: added support to manually add bookmark

---
+++ firefox/popup.js
@@ -0,0 +1,24 @@
+// Listen for form submission
+document.getElementById('bookmarkForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent default form submission
+
+    // Get the values from the form fields
+    const title = document.getElementById('title').value;
+    const url = document.getElementById('url').value;
+    const error = document.getElementById('error');
+
+    // Check if both title and URL are provided
+    if (title && url) {
+        // Use the Chrome bookmarks API to add the bookmark
+        browser.bookmarks.create({
+            title: title,
+            url: url
+        }, function(newBookmark) {
+            console.log('Bookmark added:', newBookmark);
+            error.innerText = "Status: Bookmark added";
+        });
+    } else {
+    	error.innerText = "Status: Please provide both title and URL.";
+        alert('Please provide both title and URL.');
+    }
+});
